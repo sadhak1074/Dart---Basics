c5cd284c3dd9f3f3940acb31304143830ecbfc9c
sadhak1074 <sadhaks.bcads2021@iujaipur.edu.in>
2023-06-14 14:59:51 +0530

    Booleans

---
+++ Booleans.dart
@@ -0,0 +1,11 @@
+void main() {
+  //Booleans
+  bool isTrue = true;
+  bool isFalse = false;
+
+  print("$isTrue");
+  print("$isFalse");
+
+  bool isMarried = true;
+  print("Married Status: $isMarried");
+}
