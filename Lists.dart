a07648a5054aa48b111e313de203023e13c4b878
sadhak1074 <sadhaks.bcads2021@iujaipur.edu.in>
2023-06-15 13:37:35 +0530

    Lists in dart

---
+++ Lists.dart
@@ -0,0 +1,12 @@
+void main() {
+  List<String> name = ["Sadhak", "Vishnu", "Buckieee"];
+
+  print(name[0]);
+  print(name[1]);
+  print(name[2]);
+
+  //printing the length of the list
+
+  int lenOfList = name.length;
+  print("The length of list 'name' is: $lenOfList");
+}
